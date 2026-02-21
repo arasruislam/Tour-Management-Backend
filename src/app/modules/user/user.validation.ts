e1bef8232aaffc10988de097c0bc766c8f2cb6d5
arasruislam <arasru01@gmail.com>
2026-02-21 15:30:02 +0600

    add a user update zod schema

--- src/app/modules/user/user.validation.ts
+++ src/app/modules/user/user.validation.ts
@@ -1,4 +1,5 @@
 import z from "zod";
+import { IsActive, Role } from "./user.interface";
 
 export const createUserZodSchema = z.object({
   name: z
@@ -27,5 +28,42 @@ export const createUserZodSchema = z.object({
   address: z
     .string()
     .max(200, { message: "Address must be maximum 200 characters" })
+    .optional(),
+});
+
+export const updateUserZodSchema = z.object({
+  name: z
+    .string({ error: "Name must be an string" })
+    .min(2, { message: "minimum 2 character" })
+    .max(50, { message: "maximum 50 character" })
+    .optional(),
+  password: z
+    .string()
+    .min(8, { message: "Password must be at least 8 characters long" })
+    .regex(/[A-Za-z]/, {
+      message: "Password must contain at least one letter",
+    })
+    .regex(/[0-9]/, {
+      message: "Password must contain at least one number",
+    })
+    .regex(/[^A-Za-z0-9]/, {
+      message: "Password must contain at least one symbol",
+    })
+    .optional(),
+  phone: z
+    .string()
+    .regex(/^(?:\+8801|8801|01)[3-9]\d{8}$/, {
+      message: "Phone number must be a valid Bangladesh number",
+    })
+    .optional(),
+  address: z
+    .string()
+    .max(200, { message: "Address must be maximum 200 characters" })
+    .optional(),
+  role: z.enum(Object.values(Role) as [string]).optional(),
+  isActive: z.enum(Object.values[IsActive] as [string]).optional(),
+  isDeleted: z.boolean({ error: "IsDeleted must be true or false" }).optional(),
+  isVerified: z
+    .boolean({ error: "IsVerified must be true or false" })
     .optional(),
 });
