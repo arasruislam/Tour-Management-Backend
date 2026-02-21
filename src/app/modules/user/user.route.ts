117d16e5c707a78622fc42d03e3100f801575d18
arasruislam <arasru01@gmail.com>
2026-02-21 15:14:29 +0600

    organized the zod validation & middleware

--- src/app/modules/user/user.route.ts
+++ src/app/modules/user/user.route.ts
@@ -1,45 +1,13 @@
-import { NextFunction, Request, Response, Router } from "express";
-import z from "zod";
+import { Router } from "express";
+import { validateRequest } from "../../middlewares/validateRequest";
 import { UserControllers } from "./user.controller";
+import { createUserZodSchema } from "./user.validation";
 
 const router = Router();
 
 router.post(
   "/register",
-  async (req: Request, res: Response, next: NextFunction) => {
-    const createUserZodSchema = z.object({
-      name: z
-        .string({ error: "Name must be an string" })
-        .min(2, { message: "minimum 2 character" })
-        .max(50, { message: "maximum 50 character" }),
-      email: z.string().email(),
-      password: z
-        .string()
-        .min(8, { message: "Password must be at least 8 characters long" })
-        .regex(/[A-Za-z]/, {
-          message: "Password must contain at least one letter",
-        })
-        .regex(/[0-9]/, {
-          message: "Password must contain at least one number",
-        })
-        .regex(/[^A-Za-z0-9]/, {
-          message: "Password must contain at least one symbol",
-        }),
-      phone: z
-        .string()
-        .regex(/^(?:\+8801|8801|01)[3-9]\d{8}$/, {
-          message: "Phone number must be a valid Bangladesh number",
-        })
-        .optional(),
-      address: z
-        .string()
-        .max(200, { message: "Address must be maximum 200 characters" })
-        .optional(),
-    });
-
-    req.body = await createUserZodSchema.parseAsync(req.body);
-    next();
-  },
+  validateRequest(createUserZodSchema),
   UserControllers.createUser,
 );
 router.get("/all-users", UserControllers.getAllUsers);
