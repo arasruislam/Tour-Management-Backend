78036314478eda71e875926f4191468e2dd2a6c6
arasruislam <arasru01@gmail.com>
2026-02-22 06:49:26 +0600

    verify token

--- src/app/modules/user/user.route.ts
+++ src/app/modules/user/user.route.ts
@@ -1,4 +1,7 @@
-import { Router } from "express";
+import { NextFunction, Request, Response, Router } from "express";
+import httpStatus from "http-status-codes";
+import jwt from "jsonwebtoken";
+import AppError from "../../errorHelpers/AppError";
 import { validateRequest } from "../../middlewares/validateRequest";
 import { UserControllers } from "./user.controller";
 import { createUserZodSchema } from "./user.validation";
@@ -10,6 +13,24 @@ router.post(
   validateRequest(createUserZodSchema),
   UserControllers.createUser,
 );
-router.get("/all-users", UserControllers.getAllUsers);
+router.get(
+  "/all-users",
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const accessToken = req.headers.authorization;
+      if (!accessToken) {
+        throw new AppError(httpStatus.UNAUTHORIZED, "User Unauthorized");
+      }
+
+      const verifiedToken = jwt.verify(accessToken, "secret");
+      console.log(verifiedToken);
+
+      next();
+    } catch (error) {
+      next(error);
+    }
+  },
+  UserControllers.getAllUsers,
+);
 
 export const UserRoutes = router;
