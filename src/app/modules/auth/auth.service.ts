87bca2c69265b2213bfda01954028a1628f42164
arasruislam <arasru01@gmail.com>
2026-02-22 06:36:31 +0600

    create access token for user login.

--- src/app/modules/auth/auth.service.ts
+++ src/app/modules/auth/auth.service.ts
@@ -1,5 +1,6 @@
 import bcrypt from "bcryptjs";
 import httpStatus from "http-status-codes";
+import jwt from "jsonwebtoken";
 import AppError from "../../errorHelpers/AppError";
 import { IUser } from "../user/user.interface";
 import { User } from "./../user/user.model";
@@ -11,7 +12,6 @@ const credentialsLogin = async (payload: Partial<IUser>) => {
   if (!isUserExist) {
     throw new AppError(httpStatus.BAD_REQUEST, "Email does not exist");
   }
-
   const isPasswordMatched = await bcrypt.compare(
     password as string,
     isUserExist.password as string,
@@ -20,8 +20,16 @@ const credentialsLogin = async (payload: Partial<IUser>) => {
     throw new AppError(httpStatus.BAD_REQUEST, "Password not match.");
   }
 
+  const jwtPayload = {
+    userId: isUserExist._id,
+    email: isUserExist.email,
+    role: isUserExist.role,
+  };
+  const accessToken = jwt.sign(jwtPayload, "secret", { expiresIn: "1d" });
+
   return {
     email: isUserExist.email,
+    accessToken,
   };
 };
 
